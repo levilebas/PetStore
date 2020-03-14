d7becee169e53cfff4c5664610d96c51430e4119
Levi LeBas <levilebas@gmail.com>
2020-03-15 08:39:24 +1000

    Adding XML comments

--- Unify.PetStore.Test.Services/Contract/IPetService.cs
+++ Unify.PetStore.Test.Services/Contract/IPetService.cs
@@ -6,6 +6,11 @@ namespace Unify.PetStore.Test.Services.Contract
 {
     public interface IPetService
     {
+        /// <summary>
+        /// Gets Pets by status and then groups them by category name
+        /// </summary>
+        /// <param name="status">pet status</param>
+        /// <returns></returns>
         Task<List<IGrouping<string, Pet>>> GetCategorisedPetsByStatusAsync(Anonymous status);
     }
 }
